742bd5a05f686a7b884bf36f686987885f94080d
Damian Rotta <dipiert@gmail.com>
2026-01-10 10:25:53 -0300

    WIP - extending support for more than just register-to-register MOVs

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ class Instructions(str, Enum):
 class MOVTypes(str, Enum):
     RM_FT_R = "MOV: Register/memory to/from register"
     I_T_RM = "MOV: Immediate to register/memory"
+    I_T_R = "MOV: Immediate to register"
 
 
 class _8bitRegisters(str, Enum):
@@ -57,30 +58,54 @@ class _16bitRegisters(str, Enum):
     SI = "si"
     DI = "di"
 
-class Registers(_8bitRegisters, _16bitRegisters):
-    pass
+_8BIT_REGS = {r.value for r in _8bitRegisters}
+_16BIT_REGS = {r.value for r in _16bitRegisters}
+REGISTERS = _8BIT_REGS | _16BIT_REGS
 
-#instruction_map: Dict[str, Instructions] = {
-#    ("100010", MOVTypes.RM_FT_R): Instructions.MOV,
-#    ("1100011", MOVTypes.I_T_RM): Instructions.MOV
-#}
+# instruction_map: Dict[str, Instructions] = {
+#     ("100010", MOVTypes.RM_FT_R): Instructions.MOV,
+#     ("1100011", MOVTypes.I_T_RM): Instructions.MOV,
+#     ("1011", MOVTypes.I_T_R): Instructions.MOV
+# }
 
 instruction_map : Dict[str, Instructions] = {
     "100010": MOVTypes.RM_FT_R,
     "1100011": MOVTypes.I_T_RM,
+    "1011": MOVTypes.I_T_R,
 }
 
-def is_move_type(value):
+def is_mov_type(value):
     try:
         MOVTypes(value)
         return True
     except Exception as e:
         return False
 
+def derive_mov_type(lhs, rhs):
+    """
+    Derive the MOV type based on the operands and the shift needed.
+    Depending on the MOV type, the following shift amounts are needed:
+    2 bits are needed for d and w,
+    4 bits are needed for w and reg or
+    1 bit is needed for w.
+    """    
+    if lhs in REGISTERS and rhs in REGISTERS:
+        return inv_instruction_map[Instructions.MOV][MOVTypes.RM_FT_R], 2
+    if (lhs in REGISTERS) and is_int(rhs):
+        return inv_instruction_map[Instructions.MOV][MOVTypes.I_T_R], 4
+    if ("[" in lhs or is_int(lhs)) and is_int(rhs):
+        return inv_instruction_map[Instructions.MOV][MOVTypes.I_T_RM], 1
+    if (rhs in REGISTERS) and ("[" in lhs or is_int(lhs)):
+        return inv_instruction_map[Instructions.MOV][MOVTypes.RM_FT_R], 2
+    if (lhs in REGISTERS) and ("[" in rhs or is_int(rhs)):
+        return inv_instruction_map[Instructions.MOV][MOVTypes.RM_FT_R], 2
+    raise ValueError("Unable to derive MOV type from operands.")
+
 inv_instruction_map = {
     Instructions.MOV: {
         MOVTypes.RM_FT_R: "100010",
         MOVTypes.I_T_RM: "1100011",
+        MOVTypes.I_T_R: "1011"
     }
 }
 def invert_map(mapping: Dict[Any, Any]) -> Dict[Any, Any]:
@@ -89,34 +114,34 @@ def invert_map(mapping: Dict[Any, Any]) -> Dict[Any, Any]:
 
 reg_field_encoding: Dict[Tuple[str, str], str] = {
     # (REG, W) 
-    ("000", "0"): Registers.AL,
-    ("000", "1"): Registers.AX,
-    ("001", "0"): Registers.CL,
-    ("001", "1"): Registers.CX,
-    ("010", "0"): Registers.DL,
-    ("010", "1"): Registers.DX,
-    ("011", "0"): Registers.BL,
-    ("011", "1"): Registers.BX,
-    ("100", "0"): Registers.AH,
-    ("100", "1"): Registers.SP,
-    ("101", "0"): Registers.CH,
-    ("101", "1"): Registers.BP,
-    ("110", "0"): Registers.DH,
-    ("110", "1"): Registers.SI,
-    ("111", "0"): Registers.BH,
-    ("111", "1"): Registers.DI,
+    ("000", "0"): _8bitRegisters.AL.value,
+    ("000", "1"): _16bitRegisters.AX.value,
+    ("001", "0"): _8bitRegisters.CL.value,
+    ("001", "1"): _16bitRegisters.CX.value,
+    ("010", "0"): _8bitRegisters.DL.value,
+    ("010", "1"): _16bitRegisters.DX.value,
+    ("011", "0"): _8bitRegisters.BL.value,
+    ("011", "1"): _16bitRegisters.BX.value,
+    ("100", "0"): _8bitRegisters.AH.value,
+    ("100", "1"): _16bitRegisters.SP.value,
+    ("101", "0"): _8bitRegisters.CH.value,
+    ("101", "1"): _16bitRegisters.BP.value,
+    ("110", "0"): _8bitRegisters.DH.value,
+    ("110", "1"): _16bitRegisters.SI.value,
+    ("111", "0"): _8bitRegisters.BH.value,
+    ("111", "1"): _16bitRegisters.DI.value,
 }
 
 eff_addr_encoding= {
     # (Register1, Register2): R/M
-    (Registers.BX, Registers.SI): "000",
-    (Registers.BX, Registers.DI): "001",
-    (Registers.BP, Registers.SI): "010",
-    (Registers.BP, Registers.DI): "011",
-    (Registers.SI, None): "100",
-    (Registers.DI, None): "101",
-    (Registers.BX, None): "110",        
-    (Registers.BP, None): "111",        
+    (_16bitRegisters.BX.value, _16bitRegisters.SI.value): "000",
+    (_16bitRegisters.BX.value, _16bitRegisters.DI.value): "001",
+    (_16bitRegisters.BP.value, _16bitRegisters.SI.value): "010",
+    (_16bitRegisters.BP.value, _16bitRegisters.DI.value): "011",
+    (_16bitRegisters.SI.value, None): "100",
+    (_16bitRegisters.DI.value, None): "101",
+    (_16bitRegisters.BX.value, None): "110",        
+    (_16bitRegisters.BP.value, None): "111",        
 }
 
 inv_reg_field_encoding: Dict[str, Tuple[str, str]] = invert_map(reg_field_encoding)
@@ -158,24 +183,37 @@ def _parse_instruction_line(line: str, line_num: int) -> Optional[Tuple[str, str
 
     return instruction_name, lhs, rhs
 
+def is_int(s: str) -> bool:
+    """Check if a string represents an integer."""
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
 def derive_mod_field(lhs, rhs) -> str:
     """Derive the MOD field based on the operands."""
-    if lhs in Registers and rhs in Registers:
-        # Register to register
+    if lhs in REGISTERS and rhs in REGISTERS:
+        logger.debug("Register Mode (no displacement)")
         return MODFieldEncoding.REGISTER_TO_REGISTER.value
     
-    if "[" in lhs:
-        if rhs in _8bitRegisters:
+    if "[" in lhs or is_int(lhs):
+        if rhs in _8BIT_REGS:
+            logger.debug("Memory Mode, 8-bit displacement")
             return MODFieldEncoding.REGISTER_TO_MEMORY_8BIT_DISP.value
-        elif rhs in _16bitRegisters:
+        elif rhs in _16BIT_REGS:
+            logger.debug("Memory Mode, 16-bit displacement")
             return MODFieldEncoding.REGISTER_TO_MEMORY_16BIT_DISP.value
     
-    if "[" in rhs:
-        if lhs in _8bitRegisters:
+    if "[" in rhs or is_int(rhs):
+        if lhs in _8BIT_REGS:
+            logger.debug("Memory Mode, 8-bit displacement")
             return MODFieldEncoding.REGISTER_TO_MEMORY_8BIT_DISP.value
-        elif lhs in _16bitRegisters:
+        elif lhs in _16BIT_REGS:
+            logger.debug("Memory Mode, 16-bit displacement")
             return MODFieldEncoding.REGISTER_TO_MEMORY_16BIT_DISP.value
         
+    logger.debug("Memory Mode, no displacement (except when R/M = 110, then 16-bit displacement)")
     return MODFieldEncoding.REGISTER_TO_MEMORY_NO_DISP.value
 
     
@@ -240,7 +278,7 @@ def assemble(input_path: str, output_path: str) -> None:
                                 elif in_brackets.count('+') == 2:
                                     src_add_calc_lhs, src_add_calc_rhs, disp = in_brackets.split('+')
                                     reg = eff_addr_encoding.get((src_add_calc_lhs, src_add_calc_rhs))
-                                elif in_brackets in (Registers.SI, Registers.DI, Registers.BX, Registers.BP):
+                                elif in_brackets in (REGISTERS.SI.value, REGISTERS.DI.value, REGISTERS.BX.value, REGISTERS.BP.value):
                                     reg = eff_addr_encoding.get((in_brackets, None))
                                 else:
                                     raise ValueError(f"Line: {line_num}, Invalid instruction: {line}")
@@ -263,14 +301,19 @@ def assemble(input_path: str, output_path: str) -> None:
                         logger.error("Line %d: Invalid register: %s", line_num, e)
                         sys.exit(1)
 
-                    opcode_int: int = int(inv_instruction_map[Instructions.MOV][MOVTypes.RM_FT_R], 2)  # re-visit this now that MOV has several opcodes
+                    mov_type, left_shift = derive_mov_type(lhs, rhs)
+                    opcode_int: int = int(mov_type, 2)  
                     d_int: int = 0  # direction: 0 = to REG
                     w_int: int = int(w, 2)  # width bit (0 or 1)
-                    reg_int: int = int(reg, 2)  # 3-bit reg field
-                    #rm_int: int = int(rm, 2)  # 3-bit r/m field
-
-                    first_byte: int = (opcode_int << 2) | (d_int << 1) | w_int
-                    mod_int: int = int(MOD_REGISTER_TO_REGISTER, 2)
+                    reg_int: int = int(reg, 2) 
+                    #rm_int: int = int(rm, 2)
+                    first_byte = 0
+                    if mov_type == inv_instruction_map[Instructions.MOV][MOVTypes.RM_FT_R]:
+                        first_byte: int = (opcode_int << left_shift) | (d_int << 1) | w_int # this only works for Register to register
+                    elif mov_type == inv_instruction_map[Instructions.MOV][MOVTypes.I_T_RM]:
+                        first_byte: int = (opcode_int << left_shift) | w_int | reg_int
+                    mod_type = derive_mod_field(lhs, rhs)
+                    mod_int: int = int(mod_type, 2)
                     second_byte: int = (mod_int << 6) | (reg_int << 3) #| rm_int
 
                     if not (0 <= first_byte <= 0xFF and 0 <= second_byte <= 0xFF):
@@ -281,10 +324,10 @@ def assemble(input_path: str, output_path: str) -> None:
                     output_bytes.extend([first_byte, second_byte])
 
                     debug_bin: str = format(first_byte, '08b') + format(second_byte, '08b')
-                    logger.debug("Line %d: Encoded bytes (binary): %s", line_num, debug_bin)
+                    logger.debug("Line %d: Encoded bytes (binary). First byte: %s, Second byte: %s", line_num, debug_bin[:8], debug_bin[8:])
                     logger.debug("Direction: %d, Width: %d", d_int, w_int)
                     logger.debug("Mode: %s, Reg: %s (%s)", 
-                               MOD_REGISTER_TO_REGISTER, reg, rhs)
+                               mod_type, reg, rhs)
                 else:
                     logger.warning("Line %d: Unsupported instruction: %s", line_num, instruction_name)
         
@@ -332,7 +375,7 @@ def disassemble(input_path: str, output_path: str) -> None:
                     if instruction := instruction_map.get(opcode): # disassembly breaks here - re-visit this now that MOV may have diff opcodes
                         logger.debug("Instruction %d: Identified %s instruction", instruction_count, instruction.value)
                         
-                        if is_move_type(instruction):
+                        if is_mov_type(instruction):
                             instruction = "mov" # this must be wrong, the instruction type needs must need to be used at some point.
                             d = format((word[0] >> 1) & 1, '01b')
                             w = format(word[0] & 1, '01b')
@@ -340,9 +383,9 @@ def disassemble(input_path: str, output_path: str) -> None:
                             
                             mod = format(word[1] >> 6, '02b')
                             reg_bin = format((word[1] >> 3) & 0b111, '03b')
-                            reg = reg_field_encoding.get((reg_bin, w)).value
+                            reg = reg_field_encoding.get((reg_bin, w))
                             rm_bin = format(word[1] & 0b111, '03b')
-                            rm = reg_field_encoding.get((rm_bin, w)).value
+                            rm = reg_field_encoding.get((rm_bin, w))
                             
                             logger.debug("Mode: %s, Register: %s (%s), R/M: %s (%s)", 
                                        mod, reg_bin, reg, rm_bin, rm)
